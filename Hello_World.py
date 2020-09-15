58d9792f034aaf06a3940fe869cfdacc1fe98550
Leandro <lgarruda1989@gmail.com>
2020-09-14 23:16:22 -0300

    Limpeza do arquivo Hello_World

--- Hello_World.py
+++ Hello_World.py
@@ -22,10 +22,5 @@ def Index():
 def get_multiply10(num):
     return jsonify({"Resultado":num*10})
 
-
-"""@app.route('/test')
-def hello2():
-    return jsonify({"about": "Hello World2!"})"""
-
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
